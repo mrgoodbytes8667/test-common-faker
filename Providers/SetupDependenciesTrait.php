8af9deb044e97c480ef1d5f51e164d7585e028c3
MrGoodBytes <bytes@goodbytes.live>
2021-03-30 14:35:16 -0500

    Add SetupDependenciesTrait

---
+++ Providers/SetupDependenciesTrait.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace Bytes\Common\Faker\Providers;
+
+
+use Faker\Generator;
+use Illuminate\Support\Arr;
+
+/**
+ * Trait SetupDependenciesTrait
+ * @package Bytes\Common\Faker\Providers
+ */
+trait SetupDependenciesTrait
+{
+    /**
+     * @param string $provider
+     * @param Generator $generator
+     */
+    public function addProviderIfNeeded(string $provider, Generator $generator)
+    {
+        if (is_null(Arr::first($generator->getProviders(), function ($value, $key) use ($provider) {
+            return get_class($value) === get_class($provider);
+        }))) {
+            $generator->addProvider(new $provider($generator));
+        }
+    }
+}
