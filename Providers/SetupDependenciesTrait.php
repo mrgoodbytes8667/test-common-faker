cbb95de510b2d11b66b94a7892815865bdf0b1d0
MrGoodBytes <bytes@goodbytes.live>
2021-11-06 17:02:02 -0500

    Remove dependency on illuminate/support

--- Providers/SetupDependenciesTrait.php
+++ Providers/SetupDependenciesTrait.php
@@ -5,7 +5,6 @@ namespace Bytes\Common\Faker\Providers;
 
 
 use Faker\Generator;
-use Illuminate\Support\Arr;
 
 /**
  * Trait SetupDependenciesTrait
@@ -20,10 +19,27 @@ trait SetupDependenciesTrait
      */
     public function addProviderIfNeeded(string $provider, Generator $generator)
     {
-        if (is_null(Arr::first($generator->getProviders(), function ($value, $key) use ($provider) {
+        if (is_null($this->first($generator->getProviders(), function ($value, $key) use ($provider) {
             return get_class($value) === $provider;
         }))) {
             $generator->addProvider(new $provider($generator));
         }
+    }
+
+    /**
+     * Adapted from illuminate/collections
+     * @param $array
+     * @param callable $callback
+     * @return mixed|null
+     */
+    public function first($array, callable $callback)
+    {
+        foreach ($array as $key => $value) {
+            if ($callback($value, $key)) {
+                return $value;
+            }
+        }
+
+        return null;
     }
 }
